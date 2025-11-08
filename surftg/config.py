88a6b1c3672027105f08413385df53dd7a874f2c
cyberdevlk <138901572+cyberdevlk@users.noreply.github.com>
2025-11-08 10:23:55 +0530

    Update config.py

--- surftg/config.py
+++ surftg/config.py
@@ -6,17 +6,17 @@ load_dotenv("config.env")
 
 
 class Telegram:
-    API_ID = int(getenv("API_ID", "0"))
-    API_HASH = getenv("API_HASH", "")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    API_ID = int(getenv("API_ID", "12596720"))
+    API_HASH = getenv("API_HASH", "6af95397236b2f4f0da1f29bc6cd858b")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8113380638:AAEaxturSbm2oGXQMMJCDnDzwj25uGW4Pvk")
     PORT = int(getenv("PORT", 8080))
     SESSION_STRING = getenv("SESSION_STRING", "")
-    BASE_URL = getenv("BASE_URL", "").rstrip('/') if getenv("BASE_URL") else ""
+    BASE_URL = getenv("BASE_URL", "https://cloud02.cskinglk.xyz/").rstrip('/') if getenv("BASE_URL") else ""
     DATABASE_URL = getenv("MONGODB_URI", "mongodb+srv://csclouddrive01:csclouddrive01@cluster0.x5ynaqj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     TMDB_API = getenv("TMDB_API", "")
     TMDB_LANGUAGE = getenv("TMDB_LANGUAGE", "en")
     AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL", "").split(",") if channel.strip()]
-    STATIC_AUTH_CHANNEL = [channel.strip() for channel in getenv("STATIC_AUTH_CHANNEL", "").split(",") if channel.strip()]
+    STATIC_AUTH_CHANNEL = [channel.strip() for channel in getenv("STATIC_AUTH_CHANNEL", "-1002794637424").split(",") if channel.strip()]
     THEME = getenv("THEME", "quartz").lower()
     USERS = json.loads(getenv("USERS", '{"admin": "admin"}'))
     ADMIN_USERNAME = getenv("ADMIN_USERNAME", "surfTG")
